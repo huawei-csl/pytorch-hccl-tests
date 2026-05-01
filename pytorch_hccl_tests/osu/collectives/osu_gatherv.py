77d22d382c2607a273b3a075cc26133d5ad2182f
anastasios <anastasios.zouzias@huawei.com>
2026-05-01 07:04:15 +0000

    (ci) make black happy

--- pytorch_hccl_tests/osu/collectives/osu_gatherv.py
+++ pytorch_hccl_tests/osu/collectives/osu_gatherv.py
@@ -1,14 +1,13 @@
+import logging
 import os
 import sys
 
-import logging
 import torch.distributed as dist
 
 from pytorch_hccl_tests.commons import dist_init, log_env_info
 from pytorch_hccl_tests.osu.options import Options
 from pytorch_hccl_tests.osu.osu_util_mpi import Utils
 from pytorch_hccl_tests.parser import get_parser
-
 
 logger = logging.getLogger(__name__)
 
